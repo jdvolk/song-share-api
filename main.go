fb8f78eea54a589d7810e9648be7bce35540c47b
jdvolk <justin.d.volk@gmail.com>
2020-12-24 15:26:50 -0700

    add cors support, changed data structure to be less nested

--- main.go
+++ main.go
@@ -12,14 +12,16 @@ import (
 )
 
 //Data Arrays/Globals 
-var SearchResults []Song
+var SearchResults []Song_Details
 var TimelinePosts []SongPost
-var UserFavorites []Song
+var UserFavorites []Song_Details
+
 
 // Homepage of api
 func homePage(w http.ResponseWriter, r *http.Request){
 	fmt.Fprintf(w, "Welcome to the HomePage!")
 	fmt.Println("Endpoint Hit: homePage")
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
 	w.Header().Set("Access-Control-Allow-Origin", "*")
 	if r.Method == http.MethodOptions {
 			return
@@ -30,10 +32,13 @@ func homePage(w http.ResponseWriter, r *http.Request){
 
 // Request Handler
 func handleRequests() {
-	myRouter := mux.NewRouter().StrictSlash(true)
+	myRouter := mux.NewRouter()
+	// myRouter := mux.NewRouter().StrictSlash(true)
 	myRouter.HandleFunc("/", homePage)
-	myRouter.HandleFunc("/", homePage).Methods(http.MethodGet, http.MethodPut, http.MethodPatch, http.MethodOptions)
-	myRouter.Use(mux.CORSMethodMiddleware(myRouter))
+	// myRouter.HandleFunc("/searchResults",  func(w http.ResponseWriter, r *http.Request) {
+	// 	w.Header().Set("Access-Control-Allow-Origin", "localhost:10000")
+	// }).Methods(http.MethodGet, http.MethodPut, http.MethodPatch, http.MethodOptions)
+	// myRouter.Use(mux.CORSMethodMiddleware(myRouter))
 	myRouter.HandleFunc("/Home", returnAllTimeline)
 	myRouter.HandleFunc("/Home/{ID}", addComment).Methods("PUT")
 	myRouter.HandleFunc("/Home/{ID}", returnSingleTimeline)
@@ -50,6 +55,8 @@ func handleRequests() {
 //Search Results Requests
 func returnSearchResults(w http.ResponseWriter, r *http.Request){
 	fmt.Println("Endpoint Hit: returnSearchResults")
+	// w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
 	json.NewEncoder(w).Encode(SearchResults)
 }
 
@@ -61,6 +68,7 @@ func returnSingleSearchResult(w http.ResponseWriter, r *http.Request){
 	// so this function isnt doing all of that
 	for _, song := range SearchResults {
 		if song.Song_ID == (keyInt) {
+			w.Header().Set("Access-Control-Allow-Origin", "*")
 			json.NewEncoder(w).Encode(song)
 		}
 	}
@@ -69,6 +77,7 @@ func returnSingleSearchResult(w http.ResponseWriter, r *http.Request){
 // Timeline/Home Requests
 func returnAllTimeline(w http.ResponseWriter, r *http.Request){
 	fmt.Println("Endpoint Hit: returnTimelinePosts")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
 	json.NewEncoder(w).Encode(TimelinePosts)
 }
 
@@ -80,6 +89,7 @@ func returnSingleTimeline(w http.ResponseWriter, r *http.Request){
 	// so this function isnt doing all of that
 	for _, post := range TimelinePosts {
 		if post.Post_ID == (keyInt) {
+			w.Header().Set("Access-Control-Allow-Origin", "*")
 			json.NewEncoder(w).Encode(post)
 		}
 	}
@@ -94,7 +104,8 @@ func createNewSongPost(w http.ResponseWriter, r *http.Request){
     json.Unmarshal(reqBody, &post)
     // update our global SongPost array to include
     // our new post
-    TimelinePosts = append(TimelinePosts, post)
+		TimelinePosts = append(TimelinePosts, post)
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type,Accept") 
     json.NewEncoder(w).Encode(post)
 }
 
@@ -104,6 +115,7 @@ func addComment(w http.ResponseWriter, r *http.Request) {
 	postID, _ := strconv.Atoi(id)
 	for index, song := range TimelinePosts {
 		if song.Post_ID == postID{
+			w.Header().Set("Access-Control-Allow-Headers", "Content-Type,Accept") 
 			TimelinePosts = append(TimelinePosts[:index])
 		}
 	}
@@ -112,14 +124,16 @@ func addComment(w http.ResponseWriter, r *http.Request) {
 // Favorites Requests
 func returnUserFavorites(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("Endpoint Hit: returnUserFavorites")
+	w.Header().Set("Access-Control-Allow-Headers", "Content-Type,Accept") 
 	json.NewEncoder(w).Encode(UserFavorites)
 }
 
 func addFavorite(w http.ResponseWriter, r *http.Request) {
 	reqBody, _ := ioutil.ReadAll(r.Body)
-	var song Song
+	var song Song_Details
 	json.Unmarshal(reqBody, &song)
 	UserFavorites = append(UserFavorites, song)
+	w.Header().Set("Access-Control-Allow-Headers", "Content-Type,Accept") 
 	json.NewEncoder(w).Encode(song)
 }
 
@@ -136,57 +150,48 @@ func deleteFavorite(w http.ResponseWriter, r *http.Request) {
 
 func main() {
 	// dummy data for search results
-	SearchResults = []Song{
-		Song{ 
+	SearchResults = []Song_Details{
+		Song_Details{
 			Song_ID: 1,
-			Song: Song_Details{
-				Artist: "bty cll, Botanik",
-				Song_Name: "Like a Drug",
-				Album_Cover: "https://i.scdn.co/image/ab67616d0000b273377b5deeaf095feaa44339c1",
-				IsFavorite: false,
-			},
+			Artist: "bty cll, Botanik",
+			Song_Name: "Like a Drug",
+			Album_Cover: "https://i.scdn.co/image/ab67616d0000b273377b5deeaf095feaa44339c1",
+			IsFavorite: false,
 		},
-		Song{
+		Song_Details{
 			Song_ID: 5,
-			Song: Song_Details{
-				Artist: "Louis The Child, Drew Love",
-				Song_Name: "Free",
-				Album_Cover: "https://i.scdn.co/image/ab67616d0000b273d0c97444ecc52c4ca601144a",
-				IsFavorite: false,
-			},
+			Artist: "Louis The Child, Drew Love",
+			Song_Name: "Free",
+			Album_Cover: "https://i.scdn.co/image/ab67616d0000b273d0c97444ecc52c4ca601144a",
+			IsFavorite: false,
 		},
-		Song{
+			Song_Details{
 			Song_ID: 6,
-			Song: Song_Details{
-				Artist: "bty cll",
-				Song_Name: "Here Alone",
-				Album_Cover: "https://m.media-amazon.com/images/I/71SFywf-m9L._SS500_.jpg",
-				IsFavorite: false,
-			},
+			Artist: "bty cll",
+			Song_Name: "Here Alone",
+			Album_Cover: "https://m.media-amazon.com/images/I/71SFywf-m9L._SS500_.jpg",
+			IsFavorite: false,
 		},
-		Song{
+		Song_Details{
 			Song_ID: 7,
-			Song: Song_Details{
-				Artist: "Elohim",
-				Song_Name: "Sensations - Whethan remix",
-				Album_Cover: "https://i.scdn.co/image/ab67616d0000b273b708f022a637cf80ec2f7c57",
-				IsFavorite: false,
-			},
+			Artist: "Elohim",
+			Song_Name: "Sensations - Whethan remix",
+			Album_Cover: "https://i.scdn.co/image/ab67616d0000b273b708f022a637cf80ec2f7c57",
+			IsFavorite: false,
 		},
+	
 	}
 	// Dummy Data for user posts
 	TimelinePosts = []SongPost{
 		SongPost{
 				Post_ID: 1,
-				Song : Song{
-					Song_ID : 1,
-					Song : Song_Details{
+				Song : Song_Details{
+						Song_ID : 1,
 						Artist: "bty cll, Botanik",
 						Song_Name: "Like a Drug",
 						Album_Cover: "https://i.scdn.co/image/ab67616d0000b273377b5deeaf095feaa44339c1",
 						IsFavorite: false,
-					},
-				},  
+					}, 
 				Author : Author{
 					Author : "Justin",
 					Author_ID : 2,
@@ -196,20 +201,17 @@ func main() {
 			},
 		SongPost{
 				Post_ID: 2,
-				Song : Song{
-					Song_ID : 5,
-					Song : Song_Details{
+				Song : Song_Details{
 						Artist: "Louis The Child, Coin",
 						Song_Name: "Self Care",
 						Album_Cover: "https://i.scdn.co/image/ab67616d0000b2736c6c8ec19a095e0f881b9ddd",
 						IsFavorite: false,
-					},
-				},  
+					},  
 				Author : Author{
 					Author : "Trevor",
 					Author_ID : 1,
 				},
-				Body: "Check out this song I made",
+				Body: "litty",
 				// TODO add Comments Array Here
 			},
 		
@@ -219,16 +221,17 @@ func main() {
 
 // Data Classes/Structures
 type Song_Details struct {
+	Song_ID int `json:"Song_ID"`
 	Artist string `json:"Artist"`
 	Song_Name string `json:"Song_Name"`
 	Album_Cover string `json:"Album_Cover"`
 	IsFavorite bool `json:"isFavorite"`
 }
 
-type Song struct {
-	Song_ID int `json:"Song_ID"`
-	Song Song_Details
-}
+// type Song struct {
+// 	Song_ID int `json:"Song_ID"`
+// 	Song Song_Details
+// }
 
 type Author struct {
 	Author string `json:"Author"`
@@ -243,7 +246,7 @@ type Comment struct {
 
 type SongPost struct {
 	Post_ID int `json:"Post_ID"`
-	Song Song
+	Song Song_Details
 	Author Author
 	Body string `json:"Body"`
 	Comments []Comment
